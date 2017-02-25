23da6c30e1cade89ef3047731f1dcf696be0f79f
Kevin Chen <zephyrembyr@gmail.com>
2017-02-25 15:21:34 -0500

    many new edits and consolidations

--- menu.js
+++ menu.js
@@ -1,107 +1,101 @@
 
-var menu=[],
-
     var menu = [
-        {type: app, name:  "Small Garlic-Herb Breadsticks with Marinara Sauce", price:  2.25}, 
-        {type: app, name: "Large Garlic-Herb Breadsticks with Marinara Sauce", price: 3.75}, 
-        {type: app, name: "Small Cheese Breadsticks with Marinara Sauce", price: 2.25}, 
-        {type: app, name: "Large Cheese Breadsticks with Marinara Sauce", price: 3.75}, 
-        {type: app, name: "Fried Pickles with Chipotle Mayo", price: 4.75}, 
-        {type: app, name: "Fried Veggie Eggrolls with Sweet and Sour Dipping Sauce", price: 4.95}, 
-        {type: app, name: "Hummus & Pita with Veggies", price: 3.25}, 
-        {type: app, name: "Mozzarella Sticks", price: 4.95}, 
-        {type: app, name: "Tortilla Chips and Salsa", price: 2.50}, 
-        {type: app, name: "Two Soft Pretzels", price: 2.75}, 
-        {type: app, name: "Plain Chicken Fingers", price: 5.95}, 
-        {type: app, name: "Buffalo Chicken Fingers", price: 5.95}, 
-        {type: app, name: "Buffalo Chicken Wings", price: 6.25}, 
-        {type: app, name: "BBQ Chicken Wings", price: 6.25}, 
-        {type: app, name: "Honey Mustard Chicken Wings", price: 6.25}, 
-        {type: app, name: "Monterey Jack Cheese Quesadilla", price: 4.75}, 
-        {type: app, name: "Monterey Jack Veggie Quesadilla", price: 5.50}, 
-        {type: app, name: "Monterey Jack Chicken Quesadilla", price: 5.50}, 
+        {type: "app", name:  "Small Garlic-Herb Breadsticks with Marinara Sauce", price:  2.25}, 
+        {type: "app", name: "Large Garlic-Herb Breadsticks with Marinara Sauce", price: 3.75}, 
+        {type: "app", name: "Small Cheese Breadsticks with Marinara Sauce", price: 2.25}, 
+        {type: "app", name: "Large Cheese Breadsticks with Marinara Sauce", price: 3.75}, 
+        {type: "app", name: "Fried Pickles with Chipotle Mayo", price: 4.75}, 
+        {type: "app", name: "Fried Veggie Eggrolls with Sweet and Sour Dipping Sauce", price: 4.95}, 
+        {type: "app", name: "Hummus and Pita with Veggies", price: 3.25}, 
+        {type: "app", name: "Mozzarella Sticks", price: 4.95}, 
+        {type: "app", name: "Tortilla Chips and Salsa", price: 2.50}, 
+        {type: "app", name: "Two Soft Pretzels", price: 2.75}, 
+        {type: "app", name: "Plain Chicken Fingers", price: 5.95}, 
+        {type: "app", name: "Buffalo Chicken Fingers", price: 5.95}, 
+        {type: "app", name: "Buffalo Chicken Wings", price: 6.25}, 
+        {type: "app", name: "BBQ Chicken Wings", price: 6.25}, 
+        {type: "app", name: "Honey Mustard Chicken Wings", price: 6.25}, 
+        {type: "app", name: "Monterey Jack Cheese Quesadilla", price: 4.75}, 
+        {type: "app", name: "Monterey Jack Veggie Quesadilla", price: 5.50}, 
+        {type: "app", name: "Monterey Jack Chicken Quesadilla", price: 5.50}, 
         
-        {type: salad, name: "Kale Salad", price: 6.25}, 
-        {type: salad, name: "Cobb Salad", price: 6.25}, 
-        {type: salad, name: "Cobb Salad", price: 6.25}, 
-        {type: salad, name: "Small Magee's Garden Salad", price: 2.25}, 
-        {type: salad, name: "Large Magee's Garden Salad", price: 3.75}, 
-        {type: salad, name: "Small Jack's Caesar Salad", price: 3.00}, 
-        {type: salad, name: "Large Jack's Caesar Salad", price: 4.25}, 
-        {type: salad, name: "Mandarin Almond Salad", price: 5.00}, 
-        {type: salad, name: "Greek Salad with Hummus", price: 5.25}, 
+        {type: "salad", name: "Kale Salad", price: 6.25}, 
+        {type: "salad", name: "Cobb Salad", price: 6.25}, 
+        {type: "salad", name: "Small Magee's Garden Salad", price: 2.25}, 
+        {type: "salad", name: "Large Magee's Garden Salad", price: 3.75}, 
+        {type: "salad", name: "Small Jack's Caesar Salad", price: 3.00}, 
+        {type: "salad", name: "Large Jack's Caesar Salad", price: 4.25}, 
+        {type: "salad", name: "Mandarin Almond Salad", price: 5.00}, 
+        {type: "salad", name: "Greek Salad with Hummus", price: 5.25}, 
         
-        {type: panini, name: "Chicken Pesto Panino", price: 6.25}, 
-        {type: panini, name: "Portobello & Goat Cheese Panino", price: 6.25}, 
-        {type: panini, name: "Homemade Meatloaf and Cheddar Panino", price: 6.25}, 
-        {type: panini, name: "Chicken Finger Basket with French fries", price: 6.50}, 
-        {type: panini, name: "Quinoa Chili served with a small garden salad", price: 6.25},
+        {type: "panini", name: "Chicken Pesto Panino", price: 6.25}, 
+        {type: "panini", name: "Portobello and Goat Cheese Panino", price: 6.25}, 
+        {type: "panini", name: "Homemade Meatloaf and Cheddar Panino", price: 6.25}, 
+        {type: "panini", name: "Chicken Finger Basket with French fries", price: 6.50}, 
+        {type: "panini", name: "Quinoa Chili served with a small garden salad", price: 6.25},
         
-        {type: sandwich, name: "Jack’s Deli Sandwich", price: 5.50},
-        {type: sandwich, name: "Jack’s Double-Decker Turkey Club", price: 5.75},
-        {type: sandwich, name: "BLT Sandwich", price: 4.25},
-        {type: sandwich, name: "Italian Sub", price: 5.95},
-        {type: sandwich, name: "Veggie Flatbread", price: 5.50},
-        {type: sandwich, name: "Hamburger", price: 4.00},
-        {type: sandwich, name: "Cheeseburger", price: 4.75},
-        {type: sandwich, name: "Turkey Burger", price: 4.75},
-        {type: sandwich, name: "Polar Bear Burger", price: 5.75},
-        {type: sandwich, name: "Smoke House Burger", price: 5.95},
-        {type: sandwich, name: "Blue Mango Garden Burger", price: 5.50},
-        {type: sandwich, name: "Shaved Philly Steak & Cheese", price: 5.75},
-        {type: sandwich, name: "Flat Top Falafel with Tzatziki Sauce", price: 4.75},
-        {type: sandwich, name: "Chicken Parmesan Sandwich", price: 4.75},
-        {type: sandwich, name: "Classic Grilled Cheese Sandwich", price: 3.00},
+        {type: "sandwich", name: "Jack’s Deli Sandwich", price: 5.50},
+        {type: "sandwich", name: "Jack’s Double-Decker Turkey Club", price: 5.75},
+        {type: "sandwich", name: "BLT Sandwich", price: 4.25},
+        {type: "sandwich", name: "Italian Sub", price: 5.95},
+        {type: "sandwich", name: "Veggie Flatbread", price: 5.50},
+        {type: "sandwich", name: "Hamburger", price: 4.00},
+        {type: "sandwich", name: "Cheeseburger", price: 4.75},
+        {type: "sandwich", name: "Turkey Burger", price: 4.75},
+        {type: "sandwich", name: "Polar Bear Burger", price: 5.75},
+        {type: "sandwich", name: "Smoke House Burger", price: 5.95},
+        {type: "sandwich", name: "Blue Mango Garden Burger", price: 5.50},
+        {type: "sandwich", name: "Shaved Philly Steak and Cheese", price: 5.75},
+        {type: "sandwich", name: "Flat Top Falafel with Tzatziki Sauce", price: 4.75},
+        {type: "sandwich", name: "Chicken Parmesan Sandwich", price: 4.75},
+        {type: "sandwich", name: "Classic Grilled Cheese Sandwich", price: 3.00},
         
-        {type: burrWrap, name: "Fish Taco", price: 5.95},
-        {type: burrWrap, name: "Burrito “El Grande”", price: 5.95},
-        {type: burrWrap, name: "Chicken Caesar Wrap", price: 5.25},
-        {type: burrWrap, name: "Vegetarian Caesar Salad Wrap(V)", price: 4.50}, 
-        {type: burrWrap, name: " Buffalo Chicken & Blue Cheese Wrap", price: 6.50},
-        {type: burrWrap, name: "Cobb Salad Wrap", price: 5.95},
+        {type: "burrWrap", name: "Fish Taco", price: 5.95},
+        {type: "burrWrap", name: "Burrito “El Grande”", price: 5.95},
+        {type: "burrWrap", name: "Chicken Caesar Wrap", price: 5.25},
+        {type: "burrWrap", name: "Vegetarian Caesar Salad Wrap(V)", price: 4.50}, 
+        {type: "burrWrap", name: "Buffalo Chicken and Blue Cheese Wrap", price: 6.50},
+        {type: "burrWrap", name: "Cobb Salad Wrap", price: 5.95},
         
-        {type: side, name: "Cobb Salad Wrap", price: 5.95},
-        {type: side, name: "Small French Fries", price: 1.75},
-        {type: side, name: "Large French Fries", price: 2.25},
-        {type: side, name: "Small Fajita Fries", price: 1.75},
-        {type: side, name: "Large Fajita Fries", price: 2.25},
-        {type: side, name: "Small Onion Rings", price: 1.75},
-        {type: side, name: "Large Onion Rings", price: 2.50},
-        {type: side, name: "Small Sweet Potato Fries", price: 2.25},
-        {type: side, name: "Large Sweet Potato Fries", price: 2.95},
-        {type: side, name: "Guacomole", price: 1.50}
+        {type: "side", name: "Cobb Salad Wrap", price: 5.95},
+        {type: "side", name: "Small French Fries", price: 1.75},
+        {type: "side", name: "Large French Fries", price: 2.25},
+        {type: "side", name: "Small Fajita Fries", price: 1.75},
+        {type: "side", name: "Large Fajita Fries", price: 2.25},
+        {type: "side", name: "Small Onion Rings", price: 1.75},
+        {type: "side", name: "Large Onion Rings", price: 2.50},
+        {type: "side", name: "Small Sweet Potato Fries", price: 2.25},
+        {type: "side", name: "Large Sweet Potato Fries", price: 2.95},
+        {type: "side", name: "Guacamole", price: 1.50}
         
     ];
 function getItem(index){
 	return menu[index].name;
+	console.log(menu[index].name);
 }
 
-function readData(){
-	
+var checker = [];
+
+function makeOrder(){
+	var stuff = [];
+	for(var k; k < checker.length; k++)
+	{
+		if ($('#checker[k]').is(":checked"))
+			{
+				stuff.push(checker[k]);
+			}
+	}
+	var order = {items: stuff, name: "", bowdoinID: ""};
+
 }
 
 $(#'confirm').click(storeInput);
 
 <!--Saves user input into a string object-->
-function storeInput(){
+function storeInput(order){
     var txtName = document.getElementById("txtname");
     var txtBowId = document.getElementById("txtBowId");
     var bowId = txtBowId.value;
     var name = txtName.value;
-    editObject(name, bowId);
+    
   } // end storeName
-
-function editObject(name, id)
-    {
-        order.addName(name);
-        order.addId(id);
-    }
-
-
-function addName(x){
-    object.name=x;
-}
-
-function addId(y){
-    object.Id=y;
-}
