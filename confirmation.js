66e6e5feac94c27bc5904c1bf12d27686132278d
dingnan01 <dingnan97@gmail.com>
2017-02-25 15:40:56 -0500

    update

--- confirmation.js
+++ confirmation.js
@@ -1,12 +1,14 @@
 $(#'confirm').click(storeInput);
 
-<!--Saves user input into a string object-->
+//Saves user input into a string object
 function storeInput(){
     var txtName = document.getElementById("txtname");
     var txtBowId = document.getElementById("txtBowId");
     var bowId = txtBowId.value;
     var name = txtName.value;
-    editObject(name, bowId);
+    
+    var newOrder = new Order(name, bowId, items);
+    //editObject(name, bowId);
   } // end storeName
 
 function editObject(name, id)
@@ -15,11 +17,19 @@ function editObject(name, id)
         order.addId(id);
     }
 
-
 function addName(x){
     object.name=x;
 }
 
 function addId(y){
-    object.Id=y;
+    object.id=y;
 }
+
+
+function Order(name, id, items){
+    this.name = name;
+    this.id = id;
+    this.items = items[];
+}
+
+//var newOrder = new Order(Jim, B9001, "fish taco", "coke");
