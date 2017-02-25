be26d207059acc3730b076d7d1b1014979f935bb
alicehkim <alicekim23@gmail.com>
2017-02-25 13:56:08 -0500

    changed menu format

--- menu.js
+++ menu.js
@@ -2,43 +2,40 @@
 <script src="menu.js"></script>
 var menu=[],
 
-function createMenu() {
-    
+function createMenu() {}
 
-var appetizer1 = {name:  "Small Garlic-Herb Breadsticks with Marinara Sauce", price:  2.25}, appetizer2 = {name: "Large Garlic-Herb Breadsticks with Marinara Sauce", price: 3.75}, appetizer3 = {name: "Small Cheese Breadsticks with Marinara Sauce", price: 2.25}, appetizer4 = {name: "Large Cheese Breadsticks with Marinara Sauce", price: 3.75}, appetizer5 = {name: "Fried Pickles with Chipotle Mayo", price: 4.75}, appetizer6 = {name: "Fried Veggie Eggrolls with Sweet and Sour Dipping Sauce", price: 4.95}, appetizer7 = {name: "Hummus & Pita with Veggies", price: 3.25}, appetizer8 = {name: "Mozzarella Sticks", price: 4.95}, appetizer9 = {name: "Tortilla Chips and Salsa", price: 2.50}, appetizer10 = {name: "Two Soft Pretzels", price: 2.75}, appetizer11 = {name: "Plain Chicken Fingers", price: 5.95}, appetizer12 = {name: "Buffalo Chicken Fingers", price: 5.95}, appetizer13 = {name: "Buffalo Chicken Wings", price: 6.25}, appetizer14 = {name: "BBQ Chicken Wings", price: 6.25}, appetizer15 = {name: "Honey Mustard Chicken Wings", price: 6.25}, appetizer16 = {name: "Monterey Jack Cheese Quesadilla", price: 4.75}, appetizer17 = {name: "Monterey Jack Veggie Quesadilla", price: 5.50}, appetizer18 = {name: "Monterey Jack Chicken Quesadilla", price: 5.50}, salad1 = {name: "Kale Salad", price: 6.25}, salad2 = {name: "Cobb Salad", price: 6.25}, salad2 = {name: "Cobb Salad", price: 6.25}, salad3 = {name: "Small Magee's Garden Salad", price: 2.25}, salad4 = {name: "Large Magee's Garden Salad", price: 3.75}, salad5 = {name: "Small Jack's Caesar Salad", price: 3.00}, salad6 = {name: "Large Jack's Caesar Salad", price: 4.25}, salad7 = {name: "Mandarin Almond Salad", price: 5.00}, salad8 = {name: "Greek Salad with Hummus", price: 5.25}, panini1 = {name: "Chicken Pesto Panino", price: 6.25}, panini2 = {name: "Portobello & Goat Cheese Panino", price: 6.25}, panini3 = {name: "Homemade Meatloaf and Cheddar Panino", price: 6.25}, panini4 = {name: "Chicken Finger Basket with French fries", price: 6.50}, panini5 = {name: "Quinoa Chili served with a small garden salad", price: 6.25}
-    
-    
-menu.push(appetizer1);
-menu.push(appetizer2);
-menu.push(appetizer3);
-menu.push(appetizer4);
-menu.push(appetizer5);
-menu.push(appetizer6);
-menu.push(appetizer7);
-menu.push(appetizer8);
-menu.push(appetizer9);
-menu.push(appetizer10);
-menu.push(appetizer11);
-menu.push(appetizer12);
-menu.push(appetizer13);
-menu.push(appetizer14);
-menu.push(appetizer15);
-menu.push(appetizer16);
-menu.push(appetizer17);
-menu.push(appetizer18);
-menu.push(salad1);
-menu.push(salad2);
-menu.push(salad3);
-menu.push(salad4);
-menu.push(salad5);
-menu.push(salad6);
-menu.push(salad7);
-menu.push(salad8);
-menu.push(panini1);
-menu.push(panini2);
-menu.push(panini3);
-menu.push(panini4);
-menu.push(panini5);
-
-
+    var menu = [
+        {type: app, name:  "Small Garlic-Herb Breadsticks with Marinara Sauce", price:  2.25}, 
+        {type: app, name: "Large Garlic-Herb Breadsticks with Marinara Sauce", price: 3.75}, 
+        {type: app, name: "Small Cheese Breadsticks with Marinara Sauce", price: 2.25}, 
+        {type: app, name: "Large Cheese Breadsticks with Marinara Sauce", price: 3.75}, 
+        {type: app, name: "Fried Pickles with Chipotle Mayo", price: 4.75}, 
+        {type: app, name: "Fried Veggie Eggrolls with Sweet and Sour Dipping Sauce", price: 4.95}, 
+        {type: app, name: "Hummus & Pita with Veggies", price: 3.25}, 
+        {type: app, name: "Mozzarella Sticks", price: 4.95}, 
+        {type: app, name: "Tortilla Chips and Salsa", price: 2.50}, 
+        {type: app, name: "Two Soft Pretzels", price: 2.75}, 
+        {type: app, name: "Plain Chicken Fingers", price: 5.95}, 
+        {type: app, name: "Buffalo Chicken Fingers", price: 5.95}, 
+        {type: app, name: "Buffalo Chicken Wings", price: 6.25}, 
+        {type: app, name: "BBQ Chicken Wings", price: 6.25}, 
+        {type: app, name: "Honey Mustard Chicken Wings", price: 6.25}, 
+        {type: app, name: "Monterey Jack Cheese Quesadilla", price: 4.75}, 
+        {type: app, name: "Monterey Jack Veggie Quesadilla", price: 5.50}, 
+        {type: app, name: "Monterey Jack Chicken Quesadilla", price: 5.50}, 
+        {type: salad, name: "Kale Salad", price: 6.25}, 
+        {type: salad, name: "Cobb Salad", price: 6.25}, 
+        {type: salad, name: "Cobb Salad", price: 6.25}, 
+        {type: salad, name: "Small Magee's Garden Salad", price: 2.25}, 
+        {type: salad, name: "Large Magee's Garden Salad", price: 3.75}, 
+        {type: salad, name: "Small Jack's Caesar Salad", price: 3.00}, 
+        {type: salad, name: "Large Jack's Caesar Salad", price: 4.25}, 
+        {type: salad, name: "Mandarin Almond Salad", price: 5.00}, 
+        {type: salad, name: "Greek Salad with Hummus", price: 5.25}, 
+        {type: panini, name: "Chicken Pesto Panino", price: 6.25}, 
+        {type: panini, name: "Portobello & Goat Cheese Panino", price: 6.25}, 
+        {type: panini, name: "Homemade Meatloaf and Cheddar Panino", price: 6.25}, 
+        {type: panini, name: "Chicken Finger Basket with French fries", price: 6.50}, 
+        {type: panini, name: "Quinoa Chili served with a small garden salad", price: 6.25}
+    ];
 }
