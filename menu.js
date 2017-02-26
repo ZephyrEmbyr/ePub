71c2de6ba0acc66421edbaf25095aa51227f67f4
Kevin Chen <zephyrembyr@gmail.com>
2017-02-25 23:10:06 -0500

    fixed typo

--- menu.js
+++ menu.js
@@ -138,15 +138,7 @@ $("#wow").click(makeOrder);
 
 function makeOrder(){
 	stuff = [];
-//    var checker = [a1, a2, a3, a4, a5, a6];
-//	for(var k; k < checker.length; k++)
-//	{
-//		if ($('#checker[k]').is(":checked"))
-//			{
-//				stuff.push(checker[k]);
-//			}
-//	}
-    
+
     $('input[type=checkbox]').each(function () {
         if ($(this).is(':checked'))
 			{
@@ -154,10 +146,12 @@ function makeOrder(){
 			}
     });
 	
+	changePage('confirmation');
+	
 }
 
   
-$("#finalconfirm").click(makeOrder);
+$("#finalconfirm").click(storeInput);
 
 <!--Saves user input into a string object-->
 function storeInput(order){
